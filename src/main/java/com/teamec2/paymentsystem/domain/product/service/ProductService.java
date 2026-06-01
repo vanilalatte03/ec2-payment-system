65bd0ffea4803366a447482219f032624f7a77f7
nongbu24 <pop524@naver.com>
2026-06-01 15:25:09 +0900

    refactor: 상품 조회 서비스 읽기 전용 트랜잭션 적용

--- src/main/java/com/teamec2/paymentsystem/domain/product/service/ProductService.java
+++ src/main/java/com/teamec2/paymentsystem/domain/product/service/ProductService.java
@@ -16,12 +16,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ProductService {
 
     private final ProductRepository productRepository;
