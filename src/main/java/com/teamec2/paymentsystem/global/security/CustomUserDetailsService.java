5e1b4605d9b6011147f10b14da4237125559e9fc
silverThunder09 <jeg9017@gmail.com>
2026-06-02 13:58:36 +0900

    refactor: userId 기반 사용자 조회만 사용하도록 정리

--- src/main/java/com/teamec2/paymentsystem/global/security/CustomUserDetailsService.java
+++ src/main/java/com/teamec2/paymentsystem/global/security/CustomUserDetailsService.java
@@ -5,20 +5,14 @@ import com.teamec2.paymentsystem.domain.user.repository.UserRepository;
 import com.teamec2.paymentsystem.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService {
 
     private final UserRepository userRepository;
-
-    @Override
-    public UserDetails loadUserByUsername(String userId) {
-        return loadUserById(Long.valueOf(userId));
-    }
 
     public UserDetails loadUserById(Long userId) {
         User user = userRepository.findById(userId)
